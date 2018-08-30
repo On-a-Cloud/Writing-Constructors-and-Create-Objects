a0b034b262a9573c5d1edff61e9d4fe3922cbd29
Humberto Banuelos <humberto.banuelos13@outlook.com>
2018-08-30 16:56:08 -0400

    Add project files.

---
+++ Classes/Point.cs
@@ -0,0 +1,30 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+class Point
+{
+    private int x, y;
+
+    public Point()
+    {
+        Console.WriteLine("Default constructor called");
+    }
+
+    public Point(int x, int y)
+    {
+        this.x = -1;
+        this.y = -1; 
+    }
+    public double DistanceTo(Point other)
+    {
+        int xDiff = this.x - other.x;
+        int yDiff = this.y - other.y;
+        double distance = Math.Sqrt((xDiff * xDiff) + (yDiff * yDiff));
+        return distance;
+    }
+}
